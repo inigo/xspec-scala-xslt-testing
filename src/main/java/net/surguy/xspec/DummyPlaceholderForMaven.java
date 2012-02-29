5b98af3ab844a04c8aa58cb890def2c98eb07dae
Inigo Surguy <inigo.surguy@gmail.com>
2012-02-29 21:00:47 +0000

    Added a dummy Java class, to satisfy Sonatype and allow this to be deployed to Maven Central

---
+++ src/main/java/net/surguy/xspec/DummyPlaceholderForMaven.java
@@ -0,0 +1,10 @@
+package net.surguy.xspec;
+
+/**
+ * Do-nothing class that ensures that a JavaDoc archive is created for this library, so it
+ * can be deployed to Maven Central via Sonatype (the code itself is all in Scala, documented
+ * with Scaladoc).
+ *
+ * @author Inigo Surguy
+ */
+public class DummyPlaceholderForMaven { }
